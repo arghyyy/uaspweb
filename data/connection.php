0d0fc2d959447533de828aa12726b8f169a1dd12
arghyyy <argisilaen@gmail.com>
2022-06-16 21:49:00 +0700

    c

--- data/connection.php
+++ data/connection.php
@@ -8,7 +8,7 @@ if($conn->connect_error){
     die('Connection Failed : '.$conn->connection_error);
 }else{
     $stmt = $conn->prepare("masukkan data penjualan mobil(mobil, terjual)");
-    $stmt->bind_param("", $mobil, $terjual)
+    $stmt->bind_param("", $mobil, $terjual);
     $stmt->execute();
     echo"data berhasil dimasukkan";
     $stmt->close();
