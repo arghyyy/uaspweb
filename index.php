b1e38aa06ae97692d551578926bcfedec97c436a
arghyyy <argisilaen@gmail.com>
2022-06-16 20:16:06 +0700

    change

--- index.php
+++ index.php
@@ -41,10 +41,10 @@ if(isset($_GET["edit"])){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <link href="assets/style.css">
-    <title>Data Penjualan Mobil</title>
+    <title>Data Penjualan Mobil Tahun 2021</title>
   </head>
   <body>
-    <h1 class="text-center mt-5">Data Penjualan Mobil</h1>
+    <h1 class="text-center mt-5">Data Penjualan Mobil Tahun 2021</h1>
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-10">
