ee1bf227234ea57b7750ecd5468bf3f50f2dcddf
arghyyy <argisilaen@gmail.com>
2022-06-16 21:19:16 +0700

    c

--- data/connection.php
+++ data/connection.php
@@ -4,6 +4,16 @@ $mobil = $data['mobil'];
 $terjual = $data['terjual'];
 
 $conn = new mysqli('localhost', 'root', '', 'uaspweb');
+if($conn->connect_error){
+    die('Connection Failed : '.$conn->connection_error);
+}else{
+    $stmt = $conn->prepare("masukkan data penjualan mobil(mobil, terjual)");
+    $stmt->bind_param("", $mobil, $terjual)
+    $stmt->execute();
+    echo"data berhasil dimasukkan";
+    $stmt->close();
+    $stmt->close(); 
+}
 
 // database connection to phpmyadmin
 
